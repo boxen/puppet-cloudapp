ebcd77775f93af0498469f05938b7cda9a0b0c11
Mark Tareshawty <tarebyte@gmail.com>
2014-07-26 14:55:37 -0400

    update spec and fix execution error

--- spec/classes/cloudapp_spec.rb
+++ spec/classes/cloudapp_spec.rb
@@ -2,7 +2,8 @@ require 'spec_helper'
 
 describe 'cloudapp' do
   it do
-    should contain_package('CloudApp').with({
+    should contain_package('Cloud').with({
+      :ensure   => 'installed',
       :provider => 'compressed_app',
       :source   => 'http://downloads.getcloudapp.com/mac/CloudApp-2.0.2.zip'
     })
